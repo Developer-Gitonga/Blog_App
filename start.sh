d06d2345b79327637ed961215bdf6a9496ed9b9b
Develper-Gitonga <aizensalim52@gmail.com>
2022-05-19 11:47:05 +0300

    Update

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-export SECRET_KEY=
-export DATABASE_URL='postgresql+psycopg2://@localhost:5432/pressly'
+export SECRET_KEY='dcadb4817ccfc31f2a0b'
+export DATABASE_URL='postgresql+psycopg2://dev-git:password@localhost:5432/speak'
+
+python3 manage.py server
